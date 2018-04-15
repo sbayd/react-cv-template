0b8405b84f3346bd242ce0f213d31c23e93aae08
Berkay Aydın <sbayd@users.noreply.github.com>
2018-04-15 23:28:19 +0300

    Update tags.js

--- src/data/tags.js
+++ src/data/tags.js
@@ -1,7 +1,7 @@
 export const tags = {
   sectionTitle: '',
   icon: null,
-  list: ['Javascript', 'ReactJS', 'ReactNative', 'Redux', 'C#', 'ASP.NETMVC', 'PHP', 'SOLID PRINCIPLES', 'C++', 'OPENCV', 'XAMARIN', 'ANDROID SDK', 'LARAVEL', 'SQL SERVER', 'MYSQL', 'SQLITE', 'LINQ', 'N-TIER ARCHITECTURE', 'SOA ARCHITECTURE']
+  list: ['Javascript', 'ReactJS', 'React-Native', 'Redux', 'C#', 'ASP.NET MVC', 'PHP', 'SOLID PRINCIPLES', 'XAMARIN', 'ANDROID SDK', 'SQL SERVER', 'MYSQL', 'SQLITE', 'LINQ', 'SQL', 'SOA']
 };
 
 export default tags;
