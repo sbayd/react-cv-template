179dc37693b4cddb38673dfbdc973a0b2b4d34c9
Berkay Aydın <sbayd@users.noreply.github.com>
2018-05-04 14:38:28 +0300

    Update careerProfile.js

--- src/data/careerProfile.js
+++ src/data/careerProfile.js
@@ -1,7 +1,7 @@
 export const careerProfile = {
   sectionTitle: 'Career Profile',
   icon: null,
-  description: '<p>I\'m coding something since I was 14. In the beginning I was creating game servers for popular games, after that I started with php and created some basic apps for small-sized companies. Then I decided to train myself. And now, I have successfully finished a good many projects for companies, universities and my government you can see the project details <a href="#projects">below.</a> So, may be we can work together!</p>'
+  description: '<p>I am a curios man. I\'m coding all kind of things in multiple languages, for multiple platforms since I was 14. In the beginning I was creating game servers for popular games, after that I started with php and created some basic apps for small-sized companies. Then I decided to train myself. Now, I have successfully finished a good many projects for companies, universities and my government you can see the project details <a href="#projects">below.</a> I am the Team Leader and Lead Front-End developer at JotForm now.</p>'
 };
 
 export default careerProfile;
