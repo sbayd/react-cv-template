21c95da9f4d41032b4b36ef2b82813f0b12c7373
Berkay Aydın <sbayd@users.noreply.github.com>
2018-05-04 14:49:50 +0300

    Update projects.js

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,87 +1,100 @@
 export const projects = {
-  sectionTitle: '',
-  icon: null,
-  description: 'Projects, i have completed are listed below. To get general information about private project or not-finished projects you can get in touch.',
-  categories: [
+    sectionTitle: '',
+    icon: null,
+    description: 'Projects, i have completed are listed below. To get general information about private project or not-finished projects you can get in touch.',
+    categories: [
     {
-      name: 'YD Software',
+      name: 'JotForm.com',
       list: [{
-        url: 'http:////mavimasa.ankara.bel.tr',
-        title: 'Mavi Masa',
-        description: 'Ankara Metropolitan Municipality - Mavi Masa Project - Mobile Applications (C# / Oracle / Android SDK - Java / iOS SDK/ Web Services)'
+          url: 'https:///jotform.com/build',
+          title: 'JotForm - Easiest Form Builder',
+          description: 'JotForm is the Easiest Form Builder with 3.6M users. I have worked so many parts in JotForm using React/Redux/Node.js/Design Patterns'
       },
       {
-        url: 'http:////giris.odtuteknokent.com.tr',
-        title: 'TKBS',
-        description: 'METU Technopolis Information System ( C# / SQL Server / Xamarin / MongoDB / Web Services)'
-      },
-      {
-        url: 'http:////yfyi2.odtuteknokent.com.tr',
-        title: 'YFYI',
-        description: 'New Business New Ideas(YFYİ) Entrepreneurship Acceleration Program Management System ( C# / SQL Server / Web Services)'
-      },
-      {
-        url: 'http:////ggj.odtuteknokent.com.tr',
-        title: 'Global Game Jam',
-        description: 'Global Game Jam (METU Technopolis) Management System ( C# / SQL Server / Web Services)'
-      },
-      {
-        url: 'http:////atom.odtuteknokent.com.tr',
-        title: 'ATOM in Games',
-        description: 'ATOM Pre-Incubation Center (METU Technopolis) Management System ( C# / SQL Server / Web Services)'
-      },
-      {
-        url: 'http:////sanayipolitikalari.sanayi.gov.tr',
-        title: 'Sanayi Politikalari Portali',
-        description: 'T.C. Industry Ministry Industrial Policies Portal ( C# / SQL Server / Web Services)'
-      },
-      {
-        url: 'http:////teknojump2.odtuteknokent.com.tr',
-        title: 'TeknoJUMP',
-        description: 'TeknoJUMP Entrepreneurship Acceleration Program Management System ( C# / SQL Server / Web Services)'
-      },
-      {
-        url: 'http:////bigg.odtuteknokent.com.tr',
-        title: 'TUBITAK BIGG / 1512',
-        description: 'TUBITAK Invidual Young Enterprise(BİGG/1512) Program Management System - METU ( C# / SQL Server / Web Services )'
-      },
-      {
-        url: 'http:////aosb.org.tr',
-        title: 'AOSB',
-        description: 'Ankara Chamber of Industry 1st Organized Industrial Zone Web Portal - Mobile Applications (C#, SQL Server, Android SDK - Java / iOS SDK / Web Services)'
-      },
-      {
-        url: '',
-        title: 'AMS App',
-        description: 'Central Bank of the Republic of Turkey (TCMB) Mobile Archive Management System ( Android SDK / Xamarin / Urovo SDK)'
-      },
-      {
-        url: 'http:////evdekiogretmen.bilfen.com',
-        title: 'Evdeki Ögretmen(Teacher at Home)',
-        description: 'Bilfen Evdeki Ogretmen Online Education System (C# / SQL Server / Web Services)'
-      }]
-    },
-    {
-      name: 'Extracurricular Activities',
-      list: [
-        {
-          url: '',
-          title: 'Mobile Exam System',
-          description: 'A mobile exam project for a company written with Xamarin.Android, Laravel'
+        name: 'YD Software',
+        list: [{
+          url: 'http:////mavimasa.ankara.bel.tr',
+          title: 'Mavi Masa',
+          description: 'Ankara Metropolitan Municipality - Mavi Masa Project - Mobile Applications (C# / Oracle / Android SDK - Java / iOS SDK/ Web Services)'
         },
         {
-          url: 'http:////www.karmer.hacettepe.edu.tr/hutest/',
-          title: 'RIASEC Student Career Profile',
-          description: 'Hacettepe University Career Development Application And Research Center Student Profile Application ( PHP(CI), MySQL, MongoDB)'
+          url: 'http:////giris.odtuteknokent.com.tr',
+          title: 'TKBS',
+          description: 'METU Technopolis Information System ( C# / SQL Server / Xamarin / MongoDB / Web Services)'
+        },
+        {
+          url: 'http:////yfyi2.odtuteknokent.com.tr',
+          title: 'YFYI',
+          description: 'New Business New Ideas(YFYİ) Entrepreneurship Acceleration Program Management System ( C# / SQL Server / Web Services)'
+        },
+        {
+          url: 'http:////ggj.odtuteknokent.com.tr',
+          title: 'Global Game Jam',
+          description: 'Global Game Jam (METU Technopolis) Management System ( C# / SQL Server / Web Services)'
+        },
+        {
+          url: 'http:////atom.odtuteknokent.com.tr',
+          title: 'ATOM in Games',
+          description: 'ATOM Pre-Incubation Center (METU Technopolis) Management System ( C# / SQL Server / Web Services)'
+        },
+        {
+          url: 'http:////sanayipolitikalari.sanayi.gov.tr',
+          title: 'Sanayi Politikalari Portali',
+          description: 'T.C. Industry Ministry Industrial Policies Portal ( C# / SQL Server / Web Services)'
+        },
+        {
+          url: 'http:////teknojump2.odtuteknokent.com.tr',
+          title: 'TeknoJUMP',
+          description: 'TeknoJUMP Entrepreneurship Acceleration Program Management System ( C# / SQL Server / Web Services)'
+        },
+        {
+          url: 'http:////bigg.odtuteknokent.com.tr',
+          title: 'TUBITAK BIGG / 1512',
+          description: 'TUBITAK Invidual Young Enterprise(BİGG/1512) Program Management System - METU ( C# / SQL Server / Web Services )'
+        },
+        {
+          url: 'http:////aosb.org.tr',
+          title: 'AOSB',
+          description: 'Ankara Chamber of Industry 1st Organized Industrial Zone Web Portal - Mobile Applications (C#, SQL Server, Android SDK - Java / iOS SDK / Web Services)'
         },
         {
           url: '',
-          title: 'Educational Mobile Game Framework',
-          description: 'A personal project for physical interactive mobile game development ( C#, Xamarin, Vuforia , OpenCV, MongoDB)'
-        }
-      ]
-    }
-  ]
-};
-
-export default projects;
+          title: 'AMS App',
+          description: 'Central Bank of the Republic of Turkey (TCMB) Mobile Archive Management System ( Android SDK / Xamarin / Urovo SDK)'
+        },
+        {
+          url: 'http:////yee.org.tr',
+          title: 'YEE Z-BOOK',
+          description: 'Yunus Emre Institute Z-Book Education Platform (C#/ MongoDB/ SQLite, Web Services)'
+        },
+        {
+          url: 'http:////evdekiogretmen.bilfen.com',
+          title: 'Evdeki Ögretmen(Teacher at Home)',
+          description: 'Bilfen Evdeki Ogretmen Online Education System (C# / SQL Server / Web Services)'
+        }]
+      },
+      {
+        name: 'Extracurricular Activities',
+        list: [
+          {
+            url: '',
+            title: 'Mobile Exam System',
+            description: 'A mobile exam project for a company written with Xamarin.Android, Laravel'
+          },
+          {
+            url: 'http:////www.karmer.hacettepe.edu.tr/hutest/',
+            title: 'RIASEC Student Career Profile',
+            description: 'Hacettepe University Career Development Application And Research Center Student Profile Application ( PHP(CI), MySQL, MongoDB)'
+          },
+          {
+            url: '',
+            title: 'Educational Mobile Game Framework',
+            description: 'A personal project for physical interactive mobile game development ( C#, Xamarin, Vuforia , OpenCV, MongoDB)'
+          }
+        ]
+      }
+    ]
+  };
+  
+  export default projects;
+  
